0afd8e4e4e6891a224f16792f8caee4c9f09929d
Jared Forsyth <jared@jaredforsyth.com>
2022-01-10 20:28:01 -0600

    ok, working on a little file browser, no biggie

--- App.tsx
+++ App.tsx
@@ -1,19 +1,174 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    Button,
+    StyleSheet,
+    Text,
+    View,
+    PermissionsAndroid,
+} from 'react-native';
+import TTs from 'react-native-tts';
+import {
+    copyFile,
+    ExternalDirectoryPath,
+    ExternalStorageDirectoryPath,
+    readdir,
+} from 'react-native-fs';
+import { FileBrowser } from './FileBrowser';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+    let [tick, setTick] = React.useState(0);
+    const [progress, setProgress] = React.useState(null);
+    const [error, setError] = React.useState(null);
+    React.useEffect(() => {
+        const fn = () => {
+            setTick((tick) => tick + 1);
+            setProgress(null);
+        };
+        TTs.addEventListener('tts-finish', fn);
+        const cancel = (err) => setError(err || 'Some cancel');
+        TTs.addEventListener('tts-cancel', cancel);
+        const error = (err) => setError(err || 'Some error');
+        TTs.addEventListener('tts-error', error);
+        const f2 = (evt) => setProgress(evt);
+        TTs.addEventListener('tts-progress', f2);
+        return () => {
+            TTs.removeEventListener('tts-finish', fn);
+            TTs.removeEventListener('tts-error', error);
+            TTs.removeEventListener('tts-cancel', cancel);
+            TTs.removeEventListener('tts-progress', f2);
+        };
+    }, []);
+    React.useEffect(() => {
+        PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+            {
+                title: 'Book to Audio',
+                message:
+                    'Book to Audio needs access to your external storage so it can read ebooks.',
+                buttonNeutral: 'Ask Me Later',
+                buttonNegative: 'Cancel',
+                buttonPositive: 'OK',
+            },
+        );
+    }, []);
+
+    const [pick, setPick] = React.useState(false);
+
+    if (pick) {
+        return (
+            <View style={styles.container}>
+                <FileBrowser
+                    onSelect={() => {
+                        setPick(false);
+                    }}
+                    filter={(item) =>
+                        item.isDirectory() || item.name.endsWith('.epub')
+                    }
+                />
+            </View>
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <Text>Love it {JSON.stringify(progress)}</Text>
+            <Text>Error {JSON.stringify(error)}</Text>
+            <Button
+                title="Pick a file"
+                onPress={() => {
+                    setPick(true);
+                }}
+            />
+            <Button
+                title="Hello folks"
+                onPress={() => {
+                    TTs.getInitStatus().then(() => {
+                        TTs.speak('Hello, world!');
+                    });
+                }}
+            />
+            <Text>Tick {tick}</Text>
+            <Button
+                title="Check"
+                onPress={() => {
+                    readdir(ExternalStorageDirectoryPath).then((data) => {
+                        alert(JSON.stringify(data));
+                    });
+                }}
+            />
+            <Button
+                title="Copy it over"
+                onPress={async () => {
+                    const perm = await PermissionsAndroid.check(
+                        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+                    );
+                    console.warn('perm', perm);
+                    const perm2 = await PermissionsAndroid.check(
+                        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+                    );
+                    console.warn('perm2', perm2);
+                    if (!perm) {
+                        console.warn('not granted');
+                        const res = await PermissionsAndroid.request(
+                            PermissionsAndroid.PERMISSIONS
+                                .WRITE_EXTERNAL_STORAGE,
+                        );
+                        if (res !== 'granted') {
+                            console.warn('nope', res);
+                            return;
+                        }
+                    }
+                    await copyFile(
+                        ExternalDirectoryPath + '/out.wav',
+                        ExternalStorageDirectoryPath + '/Audiobooks/out.wav',
+                    );
+                    alert('ok');
+                }}
+            />
+            <Button
+                title="Write it out!"
+                onPress={async () => {
+                    if (!(await getWritePermission())) {
+                        return;
+                    }
+                    const target =
+                        ExternalStorageDirectoryPath + '/Audiobooks/out2.wav';
+                    TTs.speakToFile('Hello my good folks!', target)
+                        .catch((err) => {
+                            console.warn('failed folks', target);
+                        })
+                        .then(() => {
+                            console.warn('ok?', target);
+                        });
+                }}
+            />
+        </View>
+    );
 }
 
+export const getWritePermission = async () => {
+    const perm = await PermissionsAndroid.check(
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+    );
+    if (!perm) {
+        console.warn('not granted');
+        const res = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+        );
+        if (res !== 'granted') {
+            console.warn('nope', res);
+            return false;
+        }
+    }
+    return true;
+};
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ FileBrowser.tsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import { Text, View, ScrollView, TouchableHighlight } from 'react-native';
+import {
+    ExternalStorageDirectoryPath,
+    readDir,
+    ReadDirItem,
+} from 'react-native-fs';
+
+export const FileBrowser = ({
+    onSelect,
+    filter,
+}: {
+    onSelect: (item: ReadDirItem) => void;
+    filter: (item: ReadDirItem) => boolean;
+}) => {
+    const [dir, setDir] = React.useState(ExternalStorageDirectoryPath);
+
+    const contents = usePromise(() => readDir(dir), [dir]);
+
+    return (
+        <ScrollView
+            style={{ flex: 1, alignSelf: 'stretch' }}
+            contentContainerStyle={{
+                alignItems: 'stretch',
+            }}
+        >
+            {contents && dir !== ExternalStorageDirectoryPath ? (
+                <TouchableHighlight
+                    onPress={() => {
+                        setDir(dir.split('/').slice(0, -1).join('/'));
+                    }}
+                >
+                    <View
+                        style={{
+                            marginHorizontal: 16,
+                            marginVertical: 8,
+                        }}
+                    >
+                        <Text
+                            style={{
+                                fontWeight: 'bold',
+                            }}
+                        >
+                            {'../'}
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+            ) : null}
+            {contents ? (
+                contents
+                    .sort((a, b) => {
+                        const d = +b.isDirectory() - +a.isDirectory();
+                        return d === 0 ? (a.name < b.name ? -1 : 1) : d;
+                    })
+                    .map((name, i) =>
+                        filter(name) ? (
+                            <TouchableHighlight
+                                key={i}
+                                onPress={() => {
+                                    if (name.isDirectory()) {
+                                        setDir(name.path);
+                                    } else {
+                                        onSelect(name);
+                                    }
+                                }}
+                            >
+                                <View
+                                    style={{
+                                        marginHorizontal: 16,
+                                        marginVertical: 8,
+                                    }}
+                                >
+                                    <Text
+                                        style={
+                                            name.isDirectory()
+                                                ? {
+                                                      fontWeight: 'bold',
+                                                  }
+                                                : {}
+                                        }
+                                    >
+                                        {name.name}
+                                    </Text>
+                                </View>
+                            </TouchableHighlight>
+                        ) : (
+                            <View
+                                key={i}
+                                style={{
+                                    marginHorizontal: 16,
+                                    marginVertical: 8,
+                                }}
+                            >
+                                <Text style={{ fontStyle: 'italic' }}>
+                                    {name.name}
+                                </Text>
+                            </View>
+                        ),
+                    )
+            ) : (
+                <Text style={{ margin: 16 }}>Loading...</Text>
+            )}
+        </ScrollView>
+    );
+};
+
+export const usePromise = <T,>(
+    fn: () => Promise<T>,
+    deps: Array<any>,
+): null | T => {
+    const [value, setValue] = React.useState(null as null | T);
+
+    React.useEffect(() => {
+        fn().then((v) => setValue(v));
+    }, deps);
+
+    return value;
+};
